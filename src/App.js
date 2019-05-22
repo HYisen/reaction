460cefb4a9903e19129b0321220f7643917f2d84
Alex <alexhyisen@gmail.com>
2019-05-22 14:03:21 +0800

    remove code not relevant to Eta2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, {Component} from 'react'
 import './App.css'
-import {Clock} from "./clock";
-import {Chart} from "./Chart";
-import {LoginControl} from "./login";
 import Main from "./Main";
 
 class App extends Component {
@@ -10,10 +7,6 @@ class App extends Component {
         return (
             <div>
                 <Main/>
-                {/*<Clock/>*/}
-                {/*<Chart width={400} height={200} showGrid={false} rankCount={5} itemCount={8} statUnit={1}*/}
-                       {/*lastRx={new Date(0)} status={false}/>*/}
-                {/*<LoginControl/>*/}
             </div>
         );
     }
